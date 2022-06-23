c39e751270baf0dbe0d5dc9c1feb7e338ed05e4e
PaperL <982713785@qq.com>
2022-06-23 12:31:34 +0800

    Initialize src

---
+++ raytracer/src/main.rs
@@ -0,0 +1,55 @@
+use std::{fs::File, process::exit};
+
+use image::{ImageBuffer, RgbImage};
+
+use console::style;
+use indicatif::{ProgressBar, ProgressStyle};
+
+fn main() {
+    print!("{}[2J", 27 as char); // Clear screen
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Set cursor position as 1,1
+
+    let height = 800;
+    let width = 800;
+    let quality = 60; // From 0 to 100
+    let path = "output/output.jpg";
+
+    println!(
+        "Image size: {}\nJPEG quality: {}",
+        style(width.to_string() + &"x".to_string() + &height.to_string()).yellow(),
+        style(quality.to_string()).yellow(),
+    );
+
+    let mut img: RgbImage = ImageBuffer::new(width, height); // Create image data
+    let progress = ProgressBar::new((height * width) as u64); // Progress bar UI powered by library `indicatif`
+    progress.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] [{pos}/{len}] ({eta})")
+        .progress_chars("#>-"));
+
+    // Generate image
+    for y in 0..height {
+        for x in 0..width {
+            let pixel_color = [
+                (y as f32 / height as f32 * 255.).floor() as u8,
+                ((x + height - y) as f32 / (height + width) as f32 * 255.).floor() as u8,
+                (x as f32 / height as f32 * 255.).floor() as u8,
+            ];
+            let pixel = img.get_pixel_mut(x, height - y - 1);
+            *pixel = image::Rgb(pixel_color);
+            progress.inc(1);
+        }
+    }
+    progress.finish();
+
+    // Output image to file
+    println!("Ouput image as \"{}\"", style(path).yellow());
+    let output_image = image::DynamicImage::ImageRgb8(img);
+    let mut output_file = File::create(path).unwrap();
+    match output_image.write_to(&mut output_file, image::ImageOutputFormat::Jpeg(quality)) {
+        Ok(_) => {}
+        // Err(_) => panic!("Outputting image fails."),
+        Err(_) => println!("{}", style("Outputting image fails.").red()),
+    }
+
+    exit(0);
+}
